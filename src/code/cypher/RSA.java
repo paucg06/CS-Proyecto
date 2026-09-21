2c104caae0236109de63a62e71ed51cea5d820cc
juaecheveste <juana.echeveste@hotmail.com>
2026-09-21 12:43:30 +0200

    RSA.java creador de claves con pruebas en el main

--- src/code/cypher/RSA.java
+++ src/code/cypher/RSA.java
@@ -3,11 +3,43 @@ package cypher; //Declarar paquete
 //Librerias
 import javax.crypto.Cipher;
 import java.util.Base64;
+import java.security.KeyPair; //para la creación de las claves públicas y privadas 
+import java.security.KeyPairGenerator; //para crear pares de claves privadas y publicas 
+import java.security.PrivateKey; 
+import java.security.PublicKey; 
+import java.security.SecureRandom; 
+import java.util.Base64; 
+
+
+
+
 
 public class RSA
 {
-    public void HelloRSA()
-    {
-        System.out.println("\n¡Hola, soy RSA.\n");
+    public static void main(String[] args){
+
+        try{
+            //generamos las claves públicas y privadas 
+            //se crea el generador de claves para el algoritmo RSA
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA"); 
+            keyGen.initialize(2048); //inicializamos el tamaño de la clave en 2048 bits 
+
+            KeyPair parDeClaves = keyGen.generateKeyPair(); 
+            PublicKey clavePublica = parDeClaves.getPublic(); 
+            PrivateKey clavePrivada = parDeClaves.getPrivate(); 
+
+            System.out.println("Prueba de la generación d elas claves"); 
+            System.out.println("Formato de la clave pública:"+ clavePublica.getFormat());
+
+            String fragmentoClave = Base64.getEncoder().encodeToString(clavePublica.getEncoded()); 
+            System.out.println("Fragmento de la clave pública: " + fragmentoClave.substring(0, 30) + "...");
+
+            String fragmentoClavePriv = Base64.getEncoder().encodeToString(clavePrivada.getEncoded()); 
+            System.out.println("Fragmento de la clave privada: " + fragmentoClavePriv.substring(0, 30) + "...");
+
+        }catch(Exception e){
+            System.err.println("No se ha podido generar la clave"); 
+        }
+
     }
 }
